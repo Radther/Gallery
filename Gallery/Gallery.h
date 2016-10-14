3316a03e26184f7fda3d9ad713b3335784e79d6f
Tom Singleton <tomquil13@gmail.com>
2016-10-14 12:16:18 +0100

    Initial Commit

---
+++ Gallery/Gallery.h
@@ -0,0 +1,19 @@
+//
+//  Gallery.h
+//  Gallery
+//
+//  Created by Tom Sinlgeton on 14/10/2016.
+//  Copyright © 2016 Tom Sinlgeton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Gallery.
+FOUNDATION_EXPORT double GalleryVersionNumber;
+
+//! Project version string for Gallery.
+FOUNDATION_EXPORT const unsigned char GalleryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Gallery/PublicHeader.h>
+
+
